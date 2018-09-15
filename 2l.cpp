2d18166f2d3b8278d699168df39f05ad86cc1bb2
Dong Zhang <nwpu_dong@hotmail.com>
2018-09-15 18:47:50 +0800

    add 8-queen alg(not complete)

---
+++ 2l.cpp
@@ -0,0 +1,11 @@
+// 8-Queens alg. 
+// Convert to full permutation of 1~8 with prefixed four number. then check each permutation -> get answer
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int queen_index_array[9]; // chess table in one array.
+
+    int *line = new int[9] 
+}
