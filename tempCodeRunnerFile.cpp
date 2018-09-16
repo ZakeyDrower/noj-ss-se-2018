9e10ee2453b4c1599b8c61fed5855d7ab436087f
Dong Zhang <nwpu_dong@hotmail.com>
2018-09-16 13:16:10 +0800

    temp file

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,109 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-long odd_len(long a)    
-{
-    long i,num,t,c;
-
-    i=a;
-    num=a;
-    t=0;
-    while(i>0)
-    {
-        c=i%10;
-        i=i/10;
-        if(t>0)
-            num=num*10+c;
-        t++;
-    }
-    if((c==1)||(c==3)||(c=7)||(c==9))
-        return num;
-    else
-        return 0;
-}
-
-int judge(long a)
-{
-    long i;
-    int flag;
-
-    flag=1;
-
-    for(i=2;i<=sqrt(a);i++)
-    {
-        if(a%i==0)
-        {
-            flag=0;
-            break;
-        }
-    }
-    if(flag)
-        return 1;
-    else
-        return 0;
-}
-
+#include<stdio.h>
+#include<stack>
+#include<string.h>
+using namespace std;
+int calcu(int num1,int num2,char op);
+stack <int> s1;
+char s[1000];
+int a[1000];
 int main()
 {
-    int a,b,p;
-    long i;
-    scanf("%d%d",&a,&b);
+ int n,i,num1,num2;
+ int result;
+ 
+ gets(s);
+ 
+ n=strlen(s);
+ 
+ for(i=0; i<n; i++)
+ {
+  if(s[i]>=48&&s[i]<=57)
+  {
+   a[i]=s[i]-'0';
+   result=a[i];
+   s1.push(a[i]);
+  }
+  else
+  {
+   num2=s1.top();
+   s1.pop();
+   num1=s1.top();
+   s1.pop();
+   result=calcu(num1,num2,s[i]);
+   s1.push(result);
+  }
+ }
+ printf("%d\n",result);
+}
 
-    if(a<100)
-    {
-        if(a<=5)
-        {
-            if(b>=11)
-            {
-                printf("5\n");
-                printf("7\n");
-                printf("11\n");
-            }
-            else if(b>=7)
-            {
-                printf("5\n");
-                printf("7\n");
-            }
-            else if(b>=5)
-            {
-                printf("5\n");
-            }
-        }
-        else if(a<=7)
-        {
-                if(b>=11)
-                {
-                    printf("7\n");
-                    printf("11\n");
-                }
-
-            else if(b>=7)
-            {
-                printf("7\n");
-            }
-        }
-        else if(a<=11)
-        {
-            if(b>=11)
-                printf("11\n");
-        }
-    }
-    for(i=10;i<9999;i++)
-    {
-        p=odd_len(i);
-
-        if(p>=a)
-        {
-            if(p>b)
-                break;
-            else
-            {
-                if(judge(p))
-                {
-                   printf("%d\n",p);
-                }
-            }
-        }
-    }
-    return 0;
+int calcu(int num1,int num2,char op)
+{
+ int result; 
+ if(op=='+')
+ {
+  result=num1+num2;
+ }
+ if(op=='-')
+ {
+  result=num1-num2;
+ }
+ if(op=='*')
+ {
+  result=num1*num2;
+ }
+ if(op=='/')
+ {
+  result=num1/num2;
+ }
+ return result;
 }
