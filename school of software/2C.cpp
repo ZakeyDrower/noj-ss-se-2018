a79a207991bb3dfb8c28d1740cf9210b7986fedd
Dong Zhang <nwpu_dong@hotmail.com>
2018-09-22 09:54:22 +0800

    更新素数判断函数

--- school of software/2C.cpp
+++ school of software/2C.cpp
@@ -1,12 +1,13 @@
 // prime number between [a, b]
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 bool is_prime_num(int num)
 {
     if(num==1)
         return false;
-    for (int i = 2; i < num; i++)
+    for (int i = 2; i < sqrt((double)num); i++)
         if(num%i == 0)
             return false;
     return true;
